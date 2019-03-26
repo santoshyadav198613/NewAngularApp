a7c9c2bd3dd2e02c4d5da575c684a463a1e441f2
Santosh Yadav <santosh.yadav198613@gmail.com>
2019-03-26 10:45:23 +0530

    added ngIf and ngFor

--- myFirstApp/projects/ecomapp/src/app/employee/employee.component.ts
+++ myFirstApp/projects/ecomapp/src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IEmployee } from './employee';
 
 @Component({
   selector: 'ecom-employee',
@@ -7,11 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  name=  'Rajesh';
+  employee: IEmployee = {
+    id: 1,
+    name: 'Test',
+    email: 'Test@gmail.com',
+    address: 'Mumbai'
+  };
+
+  empList : Array<IEmployee> = [
+    {
+      id: 1,
+      name: 'Test',
+      email: 'Test@gmail.com',
+      address: 'Mumbai'
+    },
+    {
+      id: 2,
+      name: 'Test2',
+      email: 'Test2@gmail.com',
+      address: 'Mumbai'
+    },
+    {
+      id: 3,
+      name: 'Test3',
+      email: 'Test3@gmail.com',
+      address: 'Mumbai'
+    }
+  ];
+  name = 'Rajesh';
   department = "IT";
+  isHidden = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleDiv() {
+    this.isHidden = !this.isHidden;
   }
 
 }
